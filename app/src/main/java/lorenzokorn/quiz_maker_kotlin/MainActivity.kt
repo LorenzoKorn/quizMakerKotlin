ced5d95ee71c067c984b2cbdac308c3d5cff4fb8
lorenzo korn <lkorn.9520@gmail.com>
2019-11-25 11:54:41 +0100

    Step 3: Navigation Components

--- app/src/main/java/lorenzokorn/quiz_maker_kotlin/MainActivity.kt
+++ app/src/main/java/lorenzokorn/quiz_maker_kotlin/MainActivity.kt
@@ -2,11 +2,37 @@ package lorenzokorn.quiz_maker_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        // navController
+        val navController = findNavController(R.id.navHostFragment)
+
+        //connect the navHostFragment with the actionbar
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                navController.navigateUp()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
